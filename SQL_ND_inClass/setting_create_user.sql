d5fc1103992b104a9d01a765a2011992fde6c441
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2021-12-06 21:53:08 +0900

    Update setting_create_user.sql

--- SQL_ND_inClass/setting_create_user.sql
+++ SQL_ND_inClass/setting_create_user.sql
@@ -12,3 +12,33 @@ FROM USER_TABLES;
 -- 날짜 타입 변경하기
 SELECT SYSDATE FROM DUAL;
 
+
+
+-- table 만들기 예시
+
+CREATE TABLE EMP (
+    EMP_NO VARCHAR2(30) NOT NULL,
+    EMP_NAME VARCHAR2(80) NOt NULL,
+    SALARY NUMBER       NULL,
+    HIRE_DATE DATE      NULL
+);
+
+-- 결과창
+
+명령의 1 행에서 시작하는 중 오류 발생 -
+CREATE TABLE EMP (
+    EMP_NO VARCHAR2(30) NOT NULL,
+    EMP_NAME VARCHAR2(80) NOt NULL,
+    SALARY NUMBER       NULL,
+    HIRE_DATE DATE      NULL
+)
+오류 보고 -
+ORA-01031: 권한이 불충분합니다
+01031. 00000 -  "insufficient privileges"
+*Cause:    An attempt was made to perform a database operation without
+           the necessary privileges.
+*Action:   Ask your database administrator or designated security
+           administrator to grant you the necessary privileges
+           
+           
+           
