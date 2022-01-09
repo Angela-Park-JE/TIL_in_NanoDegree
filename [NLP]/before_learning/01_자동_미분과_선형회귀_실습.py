3cb0171478e77863f0972abf9b7d8f69fbe981f9
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-01-09 20:55:30 +0900

    Update 01_자동_미분과_선형회귀_실습.py

--- [NLP]/before_learning/01_자동_미분과_선형회귀_실습.py
+++ [NLP]/before_learning/01_자동_미분과_선형회귀_실습.py
@@ -1,5 +1,5 @@
 
-# 자동 미분
+# 1. 자동 미분 #
 
 import tensorflow as tf
 
@@ -24,39 +24,39 @@ print(gradients)
 ### 결과
 # [<tf.Tensor: shape=(), dtype=float32, numpy=8.0>]
 
+
 # --- #
 
-# Implementation of linear regression using automatic differential
 
-### 학습될 가중치 변수 선언
+# 2. Implementation of linear regression using automatic differential #
+
+# 학습될 가중치 변수 선언
 w = tf.Variable(4.0)
 b = tf.Variable(1.0)
 
-### 가설을 함수로서 정의
+# 가설을 함수로서 정의
 @tf.function
 def hypothesis(x):
 	return w*x + b
 
-### 테스트 임의의 값을 넣고 출력
+# 테스트 임의의 값을 넣고 출력
 x_test = [3.5, 5, 5.5, 6]
 print(hypothesis(x_test).numpy())
 
-### 평균 제곱오차를 손실함수로 정의
+# 평균 제곱오차를 손실함수로 정의
 @tf.function
 def mse_loss(y_pred, y):
 	return tf.reduce_mean(tf.square(y_pred-y))
 	# 두 변수의 차이값을 제곱하여 평균을 취하는 함수
 
-### 이때 x와 y가 약 10배 차이나는 데이터를 넣어본다.
+# 이때 x와 y가 약 10배 차이나는 데이터를 넣어본다.
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9] # 공부하는 시간
 y = [11, 22, 33, 44, 53, 66, 77, 87, 95] # 각 공부하는 시간에 맵핑되는 성적
 
-### 옵티마이저는 경사 하강법을 사용하되, 학습률은 0.01을 사용한다.
-
+# 옵티마이저는 경사 하강법을 사용하되, 학습률은 0.01을 사용한다.
 optimizer = tf.optimizers.SGD(0.01)
 
 # 약 300번에 걸쳐 경사 하강법을 수행한다.
-
 for i in range(301):
   with tf.GradientTape() as tape:
     # 현재 파라미터에 기반한 입력 x에 대한 예측값을 y_pred
@@ -73,5 +73,4 @@ for i in range(301):
 
   if i % 10 == 0:
     print("epoch : {:3} | w의 값 : {:5.4f} | b의 값 : {:5.4} | cost : {:5.6f}".format(i, w.numpy(), b.numpy(), cost))
-
-# 각각 횟수번째(-1)와 w의 값, b의 값, 비용 함수값을 print되도록 한다.
+	# 각각 횟수번째(-1)와 w의 값, b의 값, 비용 함수값을 print되도록 한다.
