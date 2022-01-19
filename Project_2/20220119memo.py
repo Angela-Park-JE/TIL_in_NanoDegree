e35e9413c0aa8f4e625949ae9845c3f29474f424
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-01-19 22:08:10 +0900

    Create 20220119memo.py

---
+++ Project_2/20220119memo.py
@@ -0,0 +1,7 @@
+# 20220119 영화코드 목록 받아오기
+# import Beautifulsoup 
+select_one
+#old_content > table > tbody > tr:nth-child(5) > td.title > div > a
+
+select
+#old_content > table > tbody > tr:nth-child(5)
