88863a360737ec64931d98107ea7fefa9d0ce33f
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-02-09 23:23:25 +0900

    Update isin_usage.py

--- [PYTHON]_pandas_study/isin_usage.py
+++ [PYTHON]_pandas_study/isin_usage.py
@@ -1,4 +1,5 @@
 # 특정 컬럼 안에 특정 값이 있음을 확인하고 싶을 때 사용
 # DataFrame.loc[DataFrame.column_name.isin(['search_str1','search_str2',...])]
+# 찾는 값이 [] 리스트 안에 있을 경우 데이터프레임 형태로 호출한다.
 
 wine_reviews.loc[wine_reviews.country.isin(['Italy', 'France'])]
