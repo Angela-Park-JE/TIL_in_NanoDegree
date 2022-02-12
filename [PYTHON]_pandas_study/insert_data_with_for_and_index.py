5efd7f8df138b2c96c357eff0f02985ee53ea546
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-02-12 23:50:31 +0900

    Update insert_data_with_for_and_index.py

--- [PYTHON]_pandas_study/insert_data_with_for_and_index.py
+++ [PYTHON]_pandas_study/insert_data_with_for_and_index.py
@@ -1,5 +1,4 @@
-
-# 설정
+### 설정
 
 cnt = 0
 movies['영화이름'] = ''
@@ -10,7 +9,7 @@ movies['개봉일'] = ''
 movies['개봉일'][0] = action_list['개봉일'][cnt]
 
 
-# for 문
+### for 문
 # 조건에 맞을 때 변화를 주고 그렇지 않도록 한 상태이다.
 
 # movie_code 도 넣고 싶으면 movie_list와 동일하게 만들어서 
