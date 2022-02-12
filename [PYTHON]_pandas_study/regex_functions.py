0af9f699609fb194b083532aae36bff5ab1053d1
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-02-12 23:50:12 +0900

    Update regex_functions.py

--- [PYTHON]_pandas_study/regex_functions.py
+++ [PYTHON]_pandas_study/regex_functions.py
@@ -4,7 +4,6 @@
 
 ### match(pattern, string)
 # 문자열의 시작 부분부터 패턴과 매칭이 되는지 검색하는 함수
-
 pattern = r"ca"
 text = "caabsacasca"
 matchOB = re.match(pattern , text)
@@ -16,7 +15,6 @@ if matchOB:
 ### search(pattern, string)
 # 문자열에 패턴과 매칭 되는 부분이 있는지 검색하는 함수.
 # 시작 부분이 아니더라도 데려옴.
-
 pattern = r"ca"
 text = "caabsacasca"
 matchOB = re.search(pattern , text)
@@ -33,7 +31,6 @@ if matchOB:
 ### findall(pattern, string)
 # 문자열에 패턴과 매칭 되는 부분을 리스트로 전부 반환.
 # list 형태로 반환되기 때문에 group()같은 함수를 쓸 수 없다.
-
 pattern = r"ca"
 text = "caabsacasca"
 # 매칭된 값은 리스트로 모두 반환
@@ -47,7 +44,6 @@ if matchedList:
 # 문자열에 패턴과 매칭 되는 부분을 이터레이터로 반환.
 # findall 은 리스트를 반환하지만 finditer는 오브젝트 형태로 된 이터레이터를 반환하기 때문에 
 # for 문 내에서 group()같은 정보 추출 함수 등을 사용할 수 있다.
-
 pattern = r"ca"
 text = "caabsacasca"
 # 매칭된 값은 이터레이터로 모두 반환
