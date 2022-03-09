b0dfcd5a4db980d2e6be643e15666c9f6cff9c66
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-03-09 20:59:35 +0900

    Create 1219_sellers_table.sql

---
+++ Project_1/first/1219_sellers_table.sql
@@ -0,0 +1,31 @@
+-- 1. seller 주 별 셀러 수 
+SELECT "STATE", COUNT(city)
+FROM sellers
+GROUP BY "STATE";
+
+```
+RN	5
+PE	9
+AM	1
+MG	244
+RO	2
+SE	2
+CE	13
+GO	40
+MT	4
+MS	5
+ES	23
+PI	1
+MA	1
+DF	30
+RS	129
+PA	1
+PR	349
+AC	1
+SC	190
+BA	19
+PB	6
+RJ	171
+SP	1849
+```
+
